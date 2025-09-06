923f79ae991d5320832c0350a81d9132ba259894
Nagazakki <106966729+Nagazakki@users.noreply.github.com>
2025-09-06 14:10:45 +0000

    Week10 again again again

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -5,18 +5,19 @@ from .models import Author, Book, Library, Librarian
 
 # Query all books by a specific author (ForeignKey relationship)
 def query_books_by_author(author_name):
-    """Query all books by a specific author using the expected pattern"""
-    author = Author.objects.get(name=author_name)  # expected pattern
-    return Book.objects.filter(author=author)      # expected pattern
+    """Query all books by a specific author"""
+    author = Author.objects.get(name=author_name)      # expected pattern
+    return Book.objects.filter(author=author)          # expected pattern
 
 # List all books in a library (ManyToMany relationship)  
 def query_books_in_library(library_name):
     """List all books in a library"""
-    library = Library.objects.get(name=library_name)  # expected pattern
+    library = Library.objects.get(name=library_name)   # expected pattern
     return library.books.all()
 
 # Retrieve the librarian for a library (OneToOne relationship)
 def query_librarian_by_library(library_name):
     """Retrieve the librarian for a library"""
-    library = Library.objects.get(name=library_name)  # expected pattern
-    return library.librarian
+    library = Library.objects.get(name=library_name)   # expected pattern
+    librarian = Librarian.objects.get(library=library) # expected pattern
+    return librarian
