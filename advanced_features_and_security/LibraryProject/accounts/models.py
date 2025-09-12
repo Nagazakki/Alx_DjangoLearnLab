6220af6cb032c64f55acea04157fdb1acc284c27
Nagazakki <106966729+Nagazakki@users.noreply.github.com>
2025-09-12 22:55:37 +0000

    Take2

--- advanced_features_and_security/LibraryProject/accounts/models.py
+++ advanced_features_and_security/LibraryProject/accounts/models.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
 class CustomUserManager(BaseUserManager):
     """Custom user manager for CustomUser model."""
