4a0f94e72792e29875f5568068aa1a1d285ac50b
Nagazakki <106966729+Nagazakki@users.noreply.github.com>
2025-09-12 23:29:03 +0000

    Take 4.1

--- advanced_features_and_security/LibraryProject/accounts/admin.py
+++ advanced_features_and_security/LibraryProject/accounts/admin.py
@@ -1,12 +1,32 @@
 from django.contrib import admin
-
-# Register your models here.
-from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.utils.html import format_html
 from .models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 
+                   'date_of_birth', 'profile_photo_preview', 'is_staff', 'is_active')
+    
+    list_filter = UserAdmin.list_filter + ('date_of_birth',)
+    
     fieldsets = UserAdmin.fieldsets + (
-        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
+        ('Additional Info', {
+            'fields': ('date_of_birth', 'profile_photo')
+        }),
     )
+    
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Additional Info', {
+            'fields': ('date_of_birth', 'profile_photo')
+        }),
+    )
+    
+    def profile_photo_preview(self, obj):
+        if obj.profile_photo:
+            return format_html(
+                '<img src="{}" width="50" height="50" style="border-radius: 50%; object-fit: cover;" />',
+                obj.profile_photo.url
+            )
+        return "No photo"
+    profile_photo_preview.short_description = 'Profile Photo'
