6469df4c7882e7dd9b91f1b1b5a94f533f7cd616
Nagazakki <106966729+Nagazakki@users.noreply.github.com>
2025-09-20 20:43:16 +0000

    Task 0 Week 12

---
+++ api_project/api/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)   # title field
+    author = models.CharField(max_length=100)  # author field
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
