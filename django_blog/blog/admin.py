697580cf6806e7a7a654eaae8e8ec668c1a2292b
Nagazakki <106966729+Nagazakki@users.noreply.github.com>
2025-09-30 18:43:46 +0000

    Week 14 Task 1

--- django_blog/blog/admin.py
+++ django_blog/blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Profile, Post
+
+admin.site.register(Profile)
 
 # Register your models here.
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post   # import your Post model
 
-# Create your views here.
+# Home page view
+def home(request):
+    return render(request, "blog/home.html")
+
+# Post list view
+def post_list(request):
+    posts = Post.objects.all().order_by("-published_date")  # newest first
+    return render(request, "blog/post_list.html", {"posts": posts})
