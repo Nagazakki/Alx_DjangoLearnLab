be1f91de96ca0c41dd4b1d047269ef2b643a28b1
Nagazakki <106966729+Nagazakki@users.noreply.github.com>
2025-09-30 19:01:34 +0000

    Week 14 Task 1

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
-from .models import Post   # import your Post model
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import UserUpdateForm, ProfileForm
 
-# Home page view
+@login_required
+def profile_view(request):
+    if request.method == "POST":
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect("profile")
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
+
+    return render(request, "blog/profile.html", {"user_form": user_form, "profile_form": profile_form})
+
 def home(request):
     return render(request, "blog/home.html")
-
-# Post list view
 def post_list(request):
-    posts = Post.objects.all().order_by("-published_date")  # newest first
-    return render(request, "blog/post_list.html", {"posts": posts})
+    return render(request, "blog/post_list.html")
