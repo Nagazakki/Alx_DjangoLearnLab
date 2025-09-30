6ac9213fe3225f8575af4358b846789a27672121
Nagazakki <106966729+Nagazakki@users.noreply.github.com>
2025-09-30 19:15:36 +0000

    Week 14 Task 1

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -28,9 +28,9 @@ def register(request):
     if request.method == "POST":
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, "Your account has been created! You can now log in.")
-            return redirect("login")
+            user = form.save()
+            login(request, user)
+            return redirect("home")
     else:
         form = CustomUserCreationForm()
     return render(request, "registration/register.html", {"form": form})
@@ -39,16 +39,17 @@ def register(request):
 @login_required
 def profile(request):
     if request.method == "POST":
-        u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
-        if u_form.is_valid() and p_form.is_valid():
-            u_form.save()
-            p_form.save()
-            messages.success(request, "Your profile has been updated!")
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
             return redirect("profile")
     else:
-        u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileForm(instance=request.user.profile)
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
 
-    context = {"u_form": u_form, "p_form": p_form}
-    return render(request, "registration/profile.html", context)
+    return render(request, "registration/profile.html", {
+        "user_form": user_form,
+        "profile_form": profile_form,
+    })
