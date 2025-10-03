869999cb4429b39a56408379f76c5ea2e4140060
Nagazakki <106966729+Nagazakki@users.noreply.github.com>
2025-10-03 19:16:56 +0000

    Week 14 Task 3

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -22,4 +22,9 @@ urlpatterns = [
     path("post/<int:post_pk>/comments/<int:pk>/edit/", views.CommentUpdateView.as_view(), name="comment_update"),
     path("post/<int:post_pk>/comments/<int:pk>/delete/", views.CommentDeleteView.as_view(), name="comment_delete"),
     path('post/<int:pk>/comment/', CommentCreateView.as_view(), name='add-comment'),
+    # Comments CRUD
+    path("post/<int:pk>/comments/new/", views.CommentCreateView.as_view(), name="comment_create"),
+    path("comment/<int:pk>/update/", views.CommentUpdateView.as_view(), name="comment_update"),
+    path("comment/<int:pk>/delete/", views.CommentDeleteView.as_view(), name="comment_delete"),
+
 ]
