da6d74d61e2b01a3a670ce285a3601b6ba019250
Nagazakki <106966729+Nagazakki@users.noreply.github.com>
2025-10-03 20:17:49 +0000

    Week 14 Task 4

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile, Post, Comment
-
+from taggit.forms import TagWidget
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True, help_text="Required. Enter a valid email address.")
