b88894664a11450e312c08df2823264b9cf9d809
Nagazakki <106966729+Nagazakki@users.noreply.github.com>
2025-10-13 18:59:52 +0000

    Add databases to settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -6,3 +6,14 @@ ALLOWED_HOSTS = ["*"]
 SECURE_BROWSER_XSS_FILTER = True
 X_FRAME_OPTIONS = "DENY"
 SECURE_SSL_REDIRECT = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",  # You can use postgres if configured
+        "NAME": BASE_DIR / "db.sqlite3",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    }
+}
